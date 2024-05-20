23ac7d21c556e4aed4bbccd01de4fe7bd79193a1
H-Yeji <yeji980603@naver.com>
2024-05-20 11:42:45 +0900

    update

--- solve_sql/LV2/조건에 부합하는 중고거래 상태 조회하기.sql
+++ solve_sql/LV2/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -5,5 +5,8 @@ case STATUS
     else '거래완료'
 end as STATUS
 from USED_GOODS_BOARD
-where date_format(CREATED_DATE, "%Y-%m-%d") = '2022-10-05'
+where date_format(CREATED_DATE, "%Y-%m-%d") = '2022-10-05' 
 order by BOARD_ID desc;
+
+-- 이 문제 데이터 타입 확인하면 created_date 타입이 date로 되어있음
+-- 굳이 date_format 사용하지 않고 그냥 where created_date = '2022-10-05'로 해도 됨
