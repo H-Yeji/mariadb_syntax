56a6c96d4891b0a5f3acb153a774b63656dd41de
H-Yeji <yeji980603@naver.com>
2024-06-22 17:35:23 +0900

    up to 6/22 sql problem

--- solve_sql/LV2/특정 물고기를 잡은 총 수 구하기.sql
+++ solve_sql/LV2/특정 물고기를 잡은 총 수 구하기.sql
@@ -1,7 +1,5 @@
-select sum(fish_count) as FISH_COUNT
-from (select count(*) as fish_count
+select count(*) as fish_count
 from fish_name_info a
 left join fish_info b
 	on a.fish_type=b.fish_type
-where a.fish_name in('BASS', 'SNAPPER')
-group by a.fish_name) as s1;
+where a.fish_name in('BASS', 'SNAPPER');
